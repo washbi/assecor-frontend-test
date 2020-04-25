cddd48f12cef125484f9a7f7b55ceed3058d1408
Philip Nguyen <philip.nguyen@eforce21.com>
2020-04-25 20:16:21 +0200

    Film Detail: show some data

--- test-app/src/app/modules/starwars/films-detail.component/films-detail.component.ts
+++ test-app/src/app/modules/starwars/films-detail.component/films-detail.component.ts
@@ -1,14 +1,27 @@
 import {LayoutMainParentComponent} from '../layout-main-parent.component';
 import {Component, OnInit} from '@angular/core';
+import {FilmService} from '../../../shared/services/film/film.service';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {Film} from '../../../api/swapi/film';
 
 
 @Component({
   selector: 'app-films-detail',
   templateUrl: './films-detail.component.html'
 })
-export class FilmsDetailComponent extends LayoutMainParentComponent implements OnInit{
+export class FilmsDetailComponent extends LayoutMainParentComponent implements OnInit {
+
+  film: Observable<Film>;
+
+  constructor(private route: ActivatedRoute, private service: FilmService) {
+    super();
+  }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    console.log(id);
+    this.film = this.service.getById(id);
   }
 
 }
