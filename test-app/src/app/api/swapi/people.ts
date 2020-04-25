045ede2e3c1ff783a8241d77c7e0077055096a3b
Philip Nguyen <philip.nguyen@eforce21.com>
2020-04-25 17:30:50 +0200

    Swapi: people

---
+++ test-app/src/app/api/swapi/people.ts
@@ -0,0 +1,22 @@
+export interface People {
+
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: number;
+  homeworld: string;
+  mass: number;
+  name: string;
+  skin_color: string;
+  url: string;
+
+  films: string[];
+  species: string[];
+  starships: string[];
+  vehicles: string[];
+
+  created: Date;
+  edited: Date;
+
+}
---
+++ test-app/src/app/services/people/people.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {SwapiService} from '../swapi.service';
+import {Page} from '../../api/swapi/page';
+import {ResourcesEnum} from '../../api/swapi/resources.enum';
+import {People} from '../../api/swapi/people';
+
+@Injectable({ providedIn: 'root' })
+export class PeopleService extends SwapiService<People> {
+
+  all(): Promise<Page<People>> {
+    return super.all(ResourcesEnum.PEOPLE);
+  }
+
+  getById(id: number): Promise<People> {
+    return super.get(ResourcesEnum.PEOPLE, id);
+  }
+}
