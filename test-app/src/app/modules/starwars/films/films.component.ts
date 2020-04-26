bbd7da3ea813161a49e7eba464ea08b616b4c21c
Philip Nguyen <philip.nguyen@eforce21.com>
2020-04-26 13:20:53 +0200

    Films: Styling full responsive

--- test-app/src/app/modules/starwars/films/films.component.ts
+++ test-app/src/app/modules/starwars/films/films.component.ts
@@ -3,7 +3,6 @@ import {LayoutMainParentComponent} from '../layout-main-parent.component';
 import {FilmService} from '../../../shared/services/film/film.service';
 import {Film} from '../../../api/swapi/film';
 import {Observable} from 'rxjs';
-import {Router} from '@angular/router';
 import {extractId} from '../../../shared/util/extract-id-from-url';
 import {filmsLabel} from '../starwars.component';
 
@@ -17,7 +16,7 @@ export class FilmsComponent extends LayoutMainParentComponent implements OnInit 
 
   films: Observable<Film[]>;
 
-  constructor(private service: FilmService, private router: Router) {
+  constructor(private service: FilmService) {
     super();
   }
 
@@ -25,7 +24,7 @@ export class FilmsComponent extends LayoutMainParentComponent implements OnInit 
     this.films = this.service.all();
   }
 
-  goToFilm(url: string) {
-    this.router.navigate([`starwars/film/${extractId(url)}`]);
+  getFilmLink(url: string) {
+    return `/starwars/film/${extractId(url)}`;
   }
 }
